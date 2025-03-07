56033d70b57ee6878d0112e9ee10b1d749531885
mohityeole <mohit.automation93@gmail.com>
2025-03-07 11:52:27 +0530

    commiting 7 3 25

---
+++ World.java
@@ -0,0 +1,11 @@
+package One;
+
+public class World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("world");
+		System.out.println(" Hello world");
+	}
+
+}
