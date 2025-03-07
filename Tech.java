0a3cabe94b7376ef3a97fc1fdf405ad74a39103c
mohityeole <mohit.automation93@gmail.com>
2025-03-07 12:02:21 +0530

    new change 567

---
+++ Tech.java
@@ -0,0 +1,10 @@
+package One;
+
+public class Tech {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- World.java
+++ World.java
@@ -4,8 +4,9 @@ public class World {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("world");
-		System.out.println(" Hello world");
+		
+		System.out.println(" i m mohit");
+		System.out.println(" i m mohit yeole");
 	}
 
 }
